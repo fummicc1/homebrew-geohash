ed584722aad39433df33b753fa4956d872dba77e
Fumiya Tanaka <fumiya.tennis1@gmail.com>
2024-11-24 20:34:57 +0900

    Update geohash.rb

--- Formula/geohash.rb
+++ Formula/geohash.rb
@@ -1,8 +1,8 @@
 class Geohash < Formula
   desc "GeoHash implementation in Swift"
   homepage "https://github.com/fummicc1/GeoHashSwift"
-  url "https://github.com/fummicc1/GeoHashSwift/archive/refs/tags/0.0.1.tar.gz"
-  sha256 "6e97330eedd1154cfaa268daa3a07c236103e9b51f67f64d3ad69c002491342b"
+  url "https://github.com/fummicc1/GeoHashSwift/archive/refs/tags/0.0.2.tar.gz"
+  sha256 "f78d827c829b8675aa0d036e2e4e452660a5714423baf325657d700ab1e1bb12"
   license "MIT"
 
   depends_on xcode: ["16.0", :build]
